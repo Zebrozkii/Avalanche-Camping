1ca4dc8141a4db3c44cdd893e905ce8a86927a5a
Jimmy Zebroski <zebrozkii@gmail.com>
2019-05-18 20:44:56 -0700

    added another component and left note on what i plan on doing with it

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from './Navbar';
 import About from './About';
+import Home from './Home';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Home/>
       <About/>
+ 
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Home() {
+  return (
+<div>
+  <style jsx>{`
+    .container{
+
+    }
+    
+    `}</style>
+    <div>
+      <h1>Home</h1>
+      <p>Map will eventually go here but for now will put pictures of potential spots</p>
+    </div>
+</div>
+  );
+}
+
+export default Home;
